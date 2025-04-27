c190cbb132dbc16d1eede98cf1d8d7bb9eda389f
arnieS06 <sharma.arnav2016@gmail.com>
2025-04-26 18:31:11 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,3 +1,46 @@
 public class Customer {
-    
+    private String name;
+    private double loyaltyPoints;
+    private String loyaltyNumber;
+
+    public Customer(String name, String loyaltyNumber) {
+        if ((name == null || loyaltyNumber == null) || (name.isEmpty() || loyaltyNumber.isEmpty())) {
+            throw new IllegalArgumentException();
+        }
+
+        this.name = name;
+        this.loyaltyNumber = loyaltyNumber;
+    }
+
+    public void addPoints(double points) {
+        if (points < 0) {
+            throw new IllegalArgumentException("Bad input");
+        }
+
+        this.loyaltyPoints += points;
+    }
+
+    public void removePoints(double points) {
+        if (points < 0) {
+            throw new IllegalArgumentException("Bad input");
+        }
+
+        this.loyaltyPoints -= points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLoyaltyNumber() {
+        return loyaltyNumber;
+    }
+
+    public String getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public void setLoyaltyPoints(String loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
+    }
 }
